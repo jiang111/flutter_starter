fa6011566972ea88f7e7180b42c27f152dbd21ee
jyuesong <425698907@qq.com>
2023-11-24 09:33:16 +0800

    add debug tool

--- lib/test/home_page.dart
+++ lib/test/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_datetime_picker_bdaya/flutter_datetime_picker_bdaya.dart';
 import 'package:flutter_starter/base.dart';
+import 'package:flutter_starter/base/components/show_debug_boundary.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -12,83 +13,86 @@ class HomePage extends ConsumerWidget {
         title: const Text("Test"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CupertinoButton(
-                child: const Text("显示loading样式"),
-                onPressed: () {
-                  "请稍等...".loading();
-                  Future.delayed(const Duration(seconds: 3), () {
-                    "提交成功".success();
-                  });
-                }),
-            CupertinoButton(
-                child: const Text("列表分页加载"),
-                onPressed: () {
-                  appRouter.push(RoutePaths.list);
-                }),
-            "切换黑色主题"
-                .toText(
-              color: BaseTheme.of(ref).xff00ff(),
-            )
-                .click(() {
-              ref.watch(themeProvider.notifier).change(1);
-            }).padding(15.paddingAll()),
-            "切换Material3主题"
-                .toText(
-              style: BaseTheme.of(ref).xff00ff().textStyle(),
-            )
-                .click(
-              () {
-                ref.watch(themeProvider.notifier).change(2);
-              },
-            ).container(
-              padding: 15.paddingAll(),
-            ),
-            CupertinoButton(
-                child: Text(
-                  "重置主题",
-                  style: TextStyle(
-                    color: BaseTheme.of(ref).xff00ff(),
+        child: ShowDebugBoundary(
+          enabled: false,
+          child: Column(
+            children: [
+              CupertinoButton(
+                  child: const Text("显示loading样式"),
+                  onPressed: () {
+                    "请稍等...".loading();
+                    Future.delayed(const Duration(seconds: 3), () {
+                      "提交成功".success();
+                    });
+                  }),
+              CupertinoButton(
+                  child: const Text("列表分页加载"),
+                  onPressed: () {
+                    appRouter.push(RoutePaths.list);
+                  }),
+              "切换黑色主题"
+                  .toText(
+                color: BaseTheme.of(ref).xff00ff(),
+              )
+                  .click(() {
+                ref.watch(themeProvider.notifier).change(1);
+              }).padding(15.paddingAll()),
+              "切换Material3主题"
+                  .toText(
+                style: BaseTheme.of(ref).xff00ff().textStyle(),
+              )
+                  .click(
+                () {
+                  ref.watch(themeProvider.notifier).change(2);
+                },
+              ).container(
+                padding: 15.paddingAll(),
+              ),
+              CupertinoButton(
+                  child: Text(
+                    "重置主题",
+                    style: TextStyle(
+                      color: BaseTheme.of(ref).xff00ff(),
+                    ),
                   ),
-                ),
-                onPressed: () {
-                  ref.watch(themeProvider.notifier).change(0);
-                }),
-            CupertinoButton(
-                child: const Text(
-                  "网络请求日志",
-                ),
-                onPressed: () {
-                  Initial.alice.showInspector();
-                }),
-            CupertinoButton(
-              child: const Text("网络请求"),
-              onPressed: () async {
-                commit<String?>(
-                  () async {
-                    return Http().get<String>("https://newtab.work", isolate: true);
-                  },
-                  success: (t) async {
-                    t?.toast();
-                    t?.i();
-                  },
-                );
-              },
-            ),
-            "选择时间"
-                .toText(
-              style: BaseTheme.of(ref).xff00ff().textStyle(),
-            )
-                .click(
-              () async {
-                var result = await DatePickerBdaya.showDatePicker(context, locale: LocaleType.zh);
-                result?.toIso8601String().toast();
-              },
-            ).container(
-              padding: 15.paddingAll(),
-            ),
-          ],
+                  onPressed: () {
+                    ref.watch(themeProvider.notifier).change(0);
+                  }),
+              CupertinoButton(
+                  child: const Text(
+                    "网络请求日志",
+                  ),
+                  onPressed: () {
+                    Initial.alice.showInspector();
+                  }),
+              CupertinoButton(
+                child: const Text("网络请求"),
+                onPressed: () async {
+                  commit<String?>(
+                    () async {
+                      return Http().get<String>("https://newtab.work", isolate: true);
+                    },
+                    success: (t) async {
+                      t?.toast();
+                      t?.i();
+                    },
+                  );
+                },
+              ),
+              "选择时间"
+                  .toText(
+                style: BaseTheme.of(ref).xff00ff().textStyle(),
+              )
+                  .click(
+                () async {
+                  var result = await DatePickerBdaya.showDatePicker(context, locale: LocaleType.zh);
+                  result?.toIso8601String().toast();
+                },
+              ).container(
+                padding: 15.paddingAll(),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/test/splash.dart
+++ lib/test/splash.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_starter/base/router.dart';
 
+import '../base/components/defer_init.dart';
+
 class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
 
@@ -20,14 +22,24 @@ class _SplashPageState extends ConsumerState<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          "Splash Page",
-          style: TextStyle(
-            color: Color(0xFF3B3552),
-            fontSize: 14,
+      body: DeferInit(
+        create: () async {
+          //初始化服务
+          return const Center(
+            child: Text(
+              "Splash Page",
+              style: TextStyle(
+                color: Color(0xFF3B3552),
+                fontSize: 14,
+              ),
+            ),
+          );
+        },
+        emptyWidget: const Center(
+          child: Text(
+            'No Data',
           ),
         ),
       ),
